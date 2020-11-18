b4f784b0e6b595f65b095f29e6697407c028e68b
katerina <sk.katherine@gmail.com>
2020-11-18 12:33:19 +0200

    added app inside directory

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  projects: ['<rootDir>/apps/test-app', '<rootDir>/apps/my-app'],
+  projects: [
+    '<rootDir>/apps/test-app',
+    '<rootDir>/apps/my-app',
+    '<rootDir>/apps/suprematism/my-other-app',
+    '<rootDir>/apps/suprematism/my-app',
+  ],
 };
